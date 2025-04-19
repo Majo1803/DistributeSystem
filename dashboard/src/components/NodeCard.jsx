b972d0f63f85d841cbdc1f6ec74637548d085fef
María José Solís García <101850270+Majo1803@users.noreply.github.com>
2025-04-19 11:02:12 -0600

    commit4
    Css actualizado

--- dashboard/src/components/NodeCard.jsx
+++ dashboard/src/components/NodeCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function NodeCard({ node }) {
   return (
     <div className="card">
-      <h3>{node.id}</h3>
+      <h3>🖥️ {node.id}</h3>
       <p><strong>CPU:</strong> {node.cpu}%</p>
       <p><strong>RAM:</strong> {node.ram}%</p>
       <p><strong>Disco:</strong> {node.disk}%</p>
